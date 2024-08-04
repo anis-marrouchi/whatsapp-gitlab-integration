c7b207c722475ce6f76ae7b61347f0f97aec529d
Anis Marrouchi <anismarrouchi@hotmail.com>
2024-08-04 12:42:07 +0100

    chore: Update PORT in ecosystem.config.js for development and production environments

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
         watch: true,
         env: {
           NODE_ENV: "development",
-          PORT: 3000
+          PORT: 3219
         },
         env_production: {
           NODE_ENV: "production",
-          PORT: 3000
+          PORT: 3219
         }
       }
     ]
